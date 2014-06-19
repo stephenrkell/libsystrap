da91b1a1eb7a857e4a9d16513ae71553e6e8131b
Patrick Lambein <patrick@lambein.name>
2014-06-19 13:39:25 +0000

    Parse the original syscalls.h headers with bugs
    This fails to parse a header if not all arguments have names.
    
    This breaks both code generation and the main script.
    
    git-svn-id: svn+ssh://svn-rsem001.cl.cam.ac.uk/ppcmem/system-call-plumbing@812 fd1e0feb-fba0-0410-91ae-b9d66fb37e41

--- linux-x86_64/parsing/parse.hs
+++ linux-x86_64/parsing/parse.hs
@@ -4,70 +4,84 @@ module Parse where
 
 import Text.ParserCombinators.Parsec
 import Control.Monad(liftM)
-import Data.List(intercalate)
+import Data.List(intercalate, delete)
+import Data.Maybe(catMaybes)
 
--- The structure that we are meaning to parse looks like this:
--- GENTYPE sys_NAME(arg1,
---                  arg2,
---                  …
---                  argn);
+-- We mean to parse a syscall header with the general form
+--      asm lkinage long sys_ ## name ## (type0 arg0, ...);
+-- with line breaks possibly happening between arguments.
+--
+-- The general form of an argument is
+--      [type_modifier] type [__user] [*]argname
+--
+-- One example can be:
+--      asmlinkage long sys_rt_sigaction(int,
+--      				 const struct sigaction __user *,
+--      				 struct sigaction __user *,
+--      				 size_t);
+--
+-- Where __user indicates whether the memory belongs to userspace
+-- (and therefore is potentially affected by the execution of the system
+-- call).
+--
+-- We want to keep track of the system call's name, and for each
+-- argument:
+--           - its name
+--           - its type with type modifiers (not including __user)
+--           - whether it belongs to userspace
 
-data Gentype = Auto | Stub | None deriving (Show)
+data Space = User | Copy deriving (Show)
 
-data Mutability = Simple | Const | Mut deriving (Show)
-
-data Argument = Argument {mut     :: Mutability
+data Argument = Argument {sp      :: Space
                          ,argtype :: String
                          ,argname :: String
                          } deriving (Show)
 
-data Sys = Sys {gentype  :: Gentype
-               ,sys_name :: String
+data Sys = Sys {sys_name :: String
                ,args     :: [Argument]
                } deriving (Show)
 
 -- XXX Obviously this accepts invalid C symbols, but fixing it is low priority.
 c_symbol = many1 $ try alphaNum <|> char '_'
+spaces1  = many1 space
 
-file = endBy line (try (string ";\n") <|> try (string ";"))
+-- file = endBy line (try (string ";\n") <|> try (string ";"))
+file = do maybe_syscalls <- endBy blabla (char '\n')
+          return $ catMaybes maybe_syscalls
+        where blabla = do ret <- (try $ do l <- line; return $ Just l) <|> (try $ do many $ noneOf "\n"; return Nothing); return ret
 
-line = do gt <- parseGentype
-          spaces
-          string "sys_"
+line = do spaces
+          string "asmlinkage long sys_"
           name <- c_symbol
           spaces
           void <- option [] $ try $ string "(void)"
-          case void of
-                "(void)" -> return $ Sys gt name []
+          ret <- case void of
+                "(void)" -> return $ Sys name []
                 _        -> do args <- between (char '(') (char ')') parseArgs
-                               return $ Sys gt name args
+                               return $ Sys name args
+          char ';'
+          return ret
 
-parseArgs = sepBy parseArg (try (string ",\n") <|> try (string ","))
+
+parseArgs = sepBy parseArg sepArg
+        where sepArg   = (try (string ",\n") <|> try (string ","))
 
 parseArg = do spaces
-              my_mut  <- parseMutable
-              spaces
-              my_type <- liftM (intercalate " ") $
-                                many1 $ try $ c_symbol >>=
-                                    \x -> (many1 space) >> return x
-              star    <- option "" $ try (string "*")
-              my_name <- c_symbol
-              return $ Argument my_mut (my_type ++ star) my_name
-
-parseGentype = liftM choose $ many1 upper
-        where choose = \x -> case x of
-                                "AUTO" -> Auto
-                                "STUB" -> Stub
-                                "NONE" -> None
-
-parseMutable = liftM choose $ option "simple" (try (string "mut")
-                                           <|> try (string "const"))
-        where choose = \x -> case x of
-                                        "simple" -> Simple
-                                        "mut"    -> Mut
-                                        "const"  -> Const
+              types <- many1 $ try c_symbol_space
+              star  <- (try $ string "*") <|> string ""
+              argname  <- c_symbol
+              argspace <- return $ if ("__user" `elem` types)
+                                       then User else Copy
+              argtype  <- return $ (intercalate " " (delete "__user" types))
+                                       ++ star
+              return $ Argument argspace argtype argname
+           where  c_symbol_space = do ret <- c_symbol
+                                      spaces1
+                                      return $ ret
 
 parseFile = parse file "(unknown)"
-text="AUTO sys_gettid(void);\nAUTO sys_time(mut time_t __user *tloc);\nAUTO sys_stime(const time_t __user *tptr);\nAUTO sys_gettimeofday(mut struct timeval __user *tv,\nmut struct timezone __user *tz);"
+text="asmlinkage long sys_gettid(void);\nIGNORE THIS LINE\nasmlinkage long sys_time(mut time_t __user *tloc);\nasmlinkage long sys_stime(const time_t __user *tptr);\nasmlinkage long sys_gettimeofday(mut struct timeval __user *tv,\nmut struct timezone __user *tz);"
+
+parseLine = parse line "(not a line)"
 
 Right parsed = parseFile text
