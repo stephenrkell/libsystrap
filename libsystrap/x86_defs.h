09511bead37a157af4162b1d1f1956e2e45b5f12
Stephen Kell <srk31@cl.cam.ac.uk>
2016-09-12 16:02:07 +0100

    Clarify licensing terms.

--- libsystrap/x86_defs.h
+++ libsystrap/x86_defs.h
@@ -1,3 +1,30 @@
+/******************************************************************************
+ * x86_defs.h.c
+ * 
+ * Supporting definitions for
+ * generic x86 (32-bit and 64-bit) instruction decoder (NOT emulator).
+ * 
+ * Portions copyright 2016, Stephen Kell.
+ * Based on x86_emulate.h, which is
+ * Copyright (c) 2005-2007 Keir Fraser
+ * Copyright (c) 2005-2007 XenSource Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
 #include <stdint.h>
 #include <err.h> /* for warnx() */
 #include <stdlib.h>
