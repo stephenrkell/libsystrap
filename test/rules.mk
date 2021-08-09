6b3d68fc32dbcdc7023cd0f9d7e1922e1e7b794b
Stephen Kell <srk31@srcf.ucam.org>
2021-08-09 18:00:58 +0100

    test/rules.mk: invalidate result stamp if our lib has changed

--- test/rules.mk
+++ test/rules.mk
@@ -1,9 +1,11 @@
 ifneq ($(MAKECMDGOALS),clean)
 ifeq ($(MODE),preload)
+file := $(srcroot)/example/trace-syscalls.so
 PREFIX := LD_PRELOAD=$(srcroot)/example/trace-syscalls.so
 else
 ifeq ($(MODE),chain)
-PREFIX := $(srcroot)/example/trace-syscalls-ld.so
+file := $(srcroot)/example/trace-syscalls-ld.so
+PREFIX := $(file)
 else
 $(error MODE must be set to either 'chain' or 'preload')
 endif
@@ -14,6 +16,6 @@ endif
 clean::
 	rm -f run-*-*.stamp
 
-run-%-$(MODE).stamp: %
+run-%-$(MODE).stamp: % $(file)
 	$(PREFIX) ./$* && touch $@
 
