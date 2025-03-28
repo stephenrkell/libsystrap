6f2f2a6d3ef7595c1ccc296a85297631719fa6b4
Stephen Kell <srk31@srcf.ucam.org>
2025-03-28 20:59:13 +0000

    example: unbreak w.r.t. newest donald, and check bounds when indexing into syscall_names

--- example/chain.inc.c
+++ example/chain.inc.c
@@ -8,7 +8,7 @@ for (unsigned i = 0; i < inferior.ehdr.e_shnum; ++i)
 	off_t off = inferior.ehdr.e_shoff + i * inferior.ehdr.e_shentsize;
 	newloc = lseek(inferior_fd, off, SEEK_SET);
 	if (newloc != off) die("could not seek to section header %d (0x%x) in %s (fd %d)\n", i, (unsigned) off, inferior_path, inferior_fd);
-	size_t ntoread = MIN(sizeof shdrs[0], inferior.ehdr.e_shentsize);
+	size_t ntoread = (sizeof shdrs[0] <  inferior.ehdr.e_shentsize) ? sizeof shdrs[0] : inferior.ehdr.e_shentsize;
 	ssize_t nread = read(inferior_fd, &shdrs[i], ntoread);
 	if (nread != ntoread) die("could not read section header %d in %s\n", i, inferior_path);
 }
--- example/syscall-names.c
+++ example/syscall-names.c
@@ -1,10 +1,11 @@
 #include <stddef.h>
 #include <sys/syscall.h>
+#include "syscall-names.h"
 
 #define SYSCALL(nam) \
 [SYS_ ## nam] = #nam,
 
-const char *syscall_names[] = {
+const char *syscall_names[SYSCALL_MAX + 1] = {
 #include "syscall-macros.h"
 	NULL
 };
