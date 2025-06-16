e71270094177ddb24f36ad29e443ab3acc7b3253
Stephen Kell <srk31@srcf.ucam.org>
2025-06-16 20:59:19 +0100

    syscall-names.c: make a bounds error slightly less likely

--- example/syscall-names.c
+++ example/syscall-names.c
@@ -4,7 +4,11 @@
 #define SYSCALL(nam) \
 [SYS_ ## nam] = #nam,
 
-const char *syscall_names[] = {
+#ifndef SYSCALL_MAX
+#define SYSCALL_MAX 1024
+#endif
+
+const char *syscall_names[SYSCALL_MAX + 2] = {
 #include "syscall-macros.h"
 	NULL
 };
