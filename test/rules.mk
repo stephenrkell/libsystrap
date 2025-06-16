0e8aab889af2ef17e94aad96a3c13b7cb028f196
Stephen Kell <srk31@srcf.ucam.org>
2025-06-16 12:43:01 +0100

    Stop src/ and example/ 'make clean' from overlapping, and make test-running rules a bit saner

--- test/rules.mk
+++ test/rules.mk
@@ -1,21 +1,23 @@
-ifneq ($(MAKECMDGOALS),clean)
-ifeq ($(MODE),preload)
-file := $(srcroot)/example/trace-syscalls.so
-PREFIX := LD_PRELOAD=$(srcroot)/example/trace-syscalls.so
-else
-ifeq ($(MODE),chain)
-file := $(srcroot)/example/trace-syscalls-ld.so
-PREFIX := $(file)
-else
-$(error MODE must be set to either 'chain' or 'preload')
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+srcroot := $(realpath $(dir $(realpath $(THIS_MAKEFILE)))/..)
+$(info srcroot is $(srcroot))
+
+PRELOAD_BINARY := $(srcroot)/build/$(shell uname -m)/trace-syscalls.so
+CHAIN_BINARY := $(srcroot)/build/$(shell uname -m)/trace-syscalls-ld.so
+
+ifeq ($(realpath $(PRELOAD_BINARY)),)
+$(error Please build $(PRELOAD_BINARY) first)
 endif
+ifeq ($(realpath $(CHAIN_BINARY)),)
+$(error Please build $(CHAIN_BINARY) first)
 endif
-endif
+run-%-preload.stamp: % $(PRELOAD_BINARY)
+	LD_PRELOAD=$(PRELOAD_BINARY) ./$* && touch $@
+run-%-chain.stamp: % $(CHAIN_BINARY)
+	$(CHAIN_BINARY) ./$* && touch $@
+run-%.stamp: %
+	./$*
 
 .PHONY: clean
 clean::
 	rm -f run-*-*.stamp
-
-run-%-$(MODE).stamp: % $(file)
-	$(PREFIX) ./$* && touch $@
-
