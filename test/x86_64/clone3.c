0e19d256c4067ae09a6ea15e18aeaf5df886bd4e
Stephen Kell <srk31@srcf.ucam.org>
2025-06-18 22:12:23 +0100

    test: use 8kB stack size in clone tests, as have witnessed musl's vfprintf blowing a 4kB stack (within print_post_syscall)

--- test/x86_64/clone3.c
+++ test/x86_64/clone3.c
@@ -23,10 +23,10 @@ void _start(void)
 {
 	unsigned flags = CLONE_VM | CLONE_FILES | CLONE_IO | CLONE_THREAD | CLONE_SIGHAND;
 
-	void *the_stack_base = mmap(NULL, 4096, PROT_READ|PROT_WRITE,
+	void *the_stack_base = mmap(NULL, 8192, PROT_READ|PROT_WRITE,
 		MAP_PRIVATE|MAP_ANONYMOUS|MAP_GROWSDOWN, -1, 0);
 	if (the_stack_base == MAP_FAILED) exit(1);
-	void *the_stack = (void*)((uintptr_t) the_stack_base + (4096 - sizeof (void*)));
+	void *the_stack = (void*)((uintptr_t) the_stack_base + (8192 - sizeof (void*)));
 
 	typedef uint64_t u64;
 // from the man page
@@ -54,7 +54,7 @@ void _start(void)
 	struct clone_args args = (struct clone_args) {
 		.flags = ((u64) flags) & ~0xff,
 		.stack = /* LOWEST byte! */ (u64) the_stack_base,
-		.stack_size = 4096
+		.stack_size = 8192
 	};
 	fprintf(stdout, "Cloning with clone3() a thread to run %p using new 4kB stack "
 		"with topmost address %p, base %p, flags %lx\n",
